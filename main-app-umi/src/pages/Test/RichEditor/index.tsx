c83fa63922d97c62e51d62a10222e90135461fcb
Cowjiang <cowjiang@163.com>
2023-02-22 16:28:00 +0800

    修改TinyMce默认主题

--- main-app-umi/src/pages/Test/RichEditor/index.tsx
+++ main-app-umi/src/pages/Test/RichEditor/index.tsx
@@ -1,8 +1,10 @@
 import React, { useRef, useState } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import Loading from '@/components/Loading';
+import { useModel } from '@@/exports';
 
 const RichEditorTestPage: React.FC = () => {
+  const {darkTheme} = useModel('theme');
   const editorRef = useRef<any>(null);
   const log = () => {
     if (editorRef.current) {
@@ -14,13 +16,14 @@ const RichEditorTestPage: React.FC = () => {
   const initValue = '<p>初始值</p>';
 
   return (
-    <div className="w-[80vw] mx-auto">
+    <div className="mx-auto">
       <Loading
         spinning={loading}
         size="large"
       >
         <div className="h-[500px]">
           <Editor
+            key={darkTheme ? 'dark' : 'light'}
             tinymceScriptSrc="/tinymce/tinymce.min.js"
             apiKey="7r39ggx9nkeyq2y3o5scbv5en93047bmpy3221wg2tnmr3qv"
             onInit={(evt, editor) => {
@@ -31,6 +34,8 @@ const RichEditorTestPage: React.FC = () => {
             init={{
               height: 500,
               language: 'zh-Hans',
+              skin: darkTheme ? 'oxide-dark' : 'oxide',
+              content_css: darkTheme ? 'dark' : 'default',
               plugins: [
                 'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
                 'anchor', 'searchreplace', 'visualblocks', 'code',
@@ -41,14 +46,26 @@ const RichEditorTestPage: React.FC = () => {
                 'alignright alignjustify | bullist numlist outdent indent | ' +
                 'removeformat | help',
               menu: {
-                file: { title: 'File', items: '' },
-                edit: { title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall | searchreplace' },
-                view: { title: 'View', items: 'code | visualaid visualchars visualblocks | spellchecker | preview fullscreen | showcomments' },
-                insert: { title: 'Insert', items: 'image link media addcomment pageembed template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor tableofcontents | insertdatetime' },
-                format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | styles blocks fontfamily fontsize align lineheight | forecolor backcolor | language | removeformat' },
-                tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | a11ycheck code wordcount' },
-                table: { title: 'Table', items: 'inserttable | cell row column | advtablesort | tableprops deletetable' },
-                help: { title: 'Help', items: 'help' }
+                file: {title: 'File', items: ''},
+                edit: {title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall | searchreplace'},
+                view: {
+                  title: 'View',
+                  items: 'code | visualaid visualchars visualblocks | spellchecker | preview fullscreen | showcomments'
+                },
+                insert: {
+                  title: 'Insert',
+                  items: 'image link media addcomment pageembed template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor tableofcontents | insertdatetime'
+                },
+                format: {
+                  title: 'Format',
+                  items: 'bold italic underline strikethrough superscript subscript codeformat | styles blocks fontfamily fontsize align lineheight | forecolor backcolor | language | removeformat'
+                },
+                tools: {title: 'Tools', items: 'spellchecker spellcheckerlanguage | a11ycheck code wordcount'},
+                table: {
+                  title: 'Table',
+                  items: 'inserttable | cell row column | advtablesort | tableprops deletetable'
+                },
+                help: {title: 'Help', items: 'help'}
               },
               content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
               statusbar: false,
